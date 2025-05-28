2e418b428e42f11a338e92b8bf8b48bdea1e5d9b
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2025-05-28 12:55:02 +0300

    use interrupt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![no_std]
 #![no_main]
 
+use core::sync::atomic::{AtomicBool, Ordering};
+
 use defmt_rtt as _;
 use panic_probe as _;
 
 use cortex_m_rt::entry;
+use mcx_pac::interrupt;
+static FLAG_BTN_PRESSED: AtomicBool = AtomicBool::new(false);
 
 #[entry]
 fn main() -> ! {
@@ -16,6 +20,8 @@ fn main() -> ! {
             .modify(|r| {
                 r.set_PORT0_RST(true);
                 r.set_GPIO0_RST(true);
+                r.set_PORT1_RST(true);
+                r.set_GPIO1_RST(true);
             });
 
         mcx_pac::syscon::SYSCON0::instance()
@@ -24,6 +30,8 @@ fn main() -> ! {
             .modify(|r| {
                 r.set_PORT0_RST(false);
                 r.set_GPIO0_RST(false);
+                r.set_PORT1_RST(false);
+                r.set_GPIO1_RST(false);
             });
 
         // enable GPIO0 clock
@@ -33,6 +41,8 @@ fn main() -> ! {
             .modify(|ahb_ctrl0| {
                 ahb_ctrl0.set_GPIO0(true);
                 ahb_ctrl0.set_PORT0(true);
+                ahb_ctrl0.set_GPIO1(true);
+                ahb_ctrl0.set_PORT1(true);
             });
 
         mcx_pac::port::PORT0::instance()
@@ -45,39 +55,72 @@ fn main() -> ! {
                 defmt::debug!("write: {}", pcr);
             });
 
+        mcx_pac::port::PORT0::instance()
+            .regs()
+            .PCR(10)
+            .modify(|pcr| {
+                pcr.set_MUX(0);
+                pcr.set_LK(true);
+            });
+
+        mcx_pac::port::PORT1::instance()
+            .regs()
+            .PCR(3)
+            .modify(|pcr| {
+                pcr.set_MUX(0);
+                pcr.set_LK(true);
+                pcr.set_PE(false);
+                pcr.set_IBE(true);
+            });
+
         // set pin 10 in port 0 direction to output
         mcx_pac::gpio::GPIO0::instance().regs().PDDR().modify(|r| {
             r.set_PDD(10, true);
         });
 
+        mcx_pac::gpio::GPIO1::instance().regs().ICR(3).modify(|r| {
+            r.set_ISF(true);
+            r.set_IRQC(10);
+        });
+
+        mcx_pac::gpio::GPIO1::instance().regs().PDDR().modify(|r| {
+            r.set_PDD(3, false);
+        });
+
+        cortex_m::peripheral::NVIC::unmask(interrupt::GPIO10);
+        cortex_m::interrupt::enable();
+
         loop {
-            // set pin 10 in port 0 to HIGH
-            mcx_pac::gpio::GPIO0::instance()
-                .regs()
-                .PSOR()
-                .write(|gpio| {
-                    gpio.set_PTSO(10, true);
-                });
-
-            defmt::info!("off");
-
-            for _ in 0..200_000 {
-                cortex_m::asm::nop();
-            }
-
-            // set pin 10 in port 0 to LOW
-            mcx_pac::gpio::GPIO0::instance()
-                .regs()
-                .PCOR()
-                .write(|gpio| {
-                    gpio.set_PTCO(10, true);
-                });
-
-            defmt::info!("on");
-
-            for _ in 0..200_000 {
-                cortex_m::asm::nop();
+            if FLAG_BTN_PRESSED.swap(false, Ordering::Relaxed) {
+                mcx_pac::gpio::GPIO0::instance()
+                    .regs()
+                    .PTOR()
+                    .write(|gpio| {
+                        gpio.set_PTTO(10, true);
+                    });
             }
         }
     }
+}
+
+#[interrupt]
+unsafe fn GPIO10() {
+    defmt::info!("GPIO10");
+
+    unsafe {
+        mcx_pac::gpio::GPIO1::instance()
+            .regs()
+            .ICR(3)
+            .modify(|r| r.set_ISF(true));
+    }
+
+    // cortex_m::interrupt::free(|cs| {
+    //     BTN.borrow(cs)
+    //         .borrow_mut()
+    //         .as_mut()
+    //         .unwrap()
+    //         .clear_interrupt_flag()
+    // });
+
+    FLAG_BTN_PRESSED.store(true, Ordering::Relaxed);
 }
